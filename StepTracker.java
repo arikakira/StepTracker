27ad42f1eee03ac165c2f0679248e6d45c062456
arikakira <samanthal242@nycstudents.net>
2025-04-12 03:28:55 +0000

    Update

--- StepTracker.java
+++ StepTracker.java
@@ -1,1 +1,29 @@
+public class StepTracker {
+    private int goal;
+    private int totalSteps;
+    private int days;
+    private int activeDays;
 
+    public StepTracker(int num) {
+        goal = num;
+    }
+
+    public int activeDays() {
+        return activeDays;
+    }
+
+    public double averageSteps() {
+        if(days==0) {
+            return 0;
+        }
+        return (double) totalSteps/days;
+    }
+
+    public void addDailySteps(int num) {
+        days++;
+        totalSteps += num;
+        if(num >= goal) {
+            activeDays++;
+        }
+    }
+}
